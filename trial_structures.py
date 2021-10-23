e1a77bb57bfff1ce9a07f4729a72f583465f6074
matt m <matthew.mccann13@gmail.com>
2021-10-23 14:05:07 +0200

    VR tuning trial structure

--- trial_structures.py
+++ trial_structures.py
@@ -61,3 +61,52 @@ class VRScreenTrialStructure:
         trial_times = [d / s for d, s in zip(distances, speeds)]
         duration = total_isi + sum(trial_times)
         return duration
+
+
+class VRTuningTrialStructure:
+    """A class to handle the trial structure of the VR Tuning experiment"""
+
+    def __init__(self, trials, trial_duration=2, isi=1):
+        # These variables are all attributes of the trial structure class so they can be modified by calls to
+        # functions from different threads
+        self.df = trials
+        self.isi = isi    # seconds
+        self.trial_duration = trial_duration     # sec
+
+        self.in_session = True
+        self.start_trial = True
+
+        self.num_trials = len(self.df)
+        self.trial_idx = 0
+        self.duration = self.calculate_duration()
+
+    def handshake(self, *values):
+        """Function for debugging osc communication"""
+        print("Trial {} received".format(values[0]))
+
+    def end_trial(self, *values):
+        """Receives message for trial end and increments to next trial"""
+        print("Trial {} completed\n".format(values[0]))
+
+        if self.trial_idx < self.num_trials - 1:
+            self.trial_idx += 1
+            self.start_trial = True
+        else:
+            self.in_session = False
+
+    def assemble_setup_message(self):
+        setup_message = [str(self.trial_duration), str(self.isi)]
+        return setup_message
+
+    def assemble_trial_message(self):
+        """Assemble the OSC message to get sent to Unity"""
+        row = self.df.iloc[self.trial_idx].to_list()
+        trial_message = [int(self.trial_idx + 1)] + row
+        trial_message = [str(tm) for tm in trial_message]
+        return trial_message
+
+    def calculate_duration(self):
+        total_isi = self.isi * (self.num_trials - 1)
+        total_trial_time = self.trial_duration * (self.num_trials - 1)
+        duration = total_isi + total_trial_time
+        return duration
