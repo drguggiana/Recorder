7b80365a5f8f6e5b8ef551e75619d511273518b4
mkm4884 <matthew.mccann13@gmail.com>
2021-11-17 11:51:05 +0100

    Updated exerimental parameters. Added functionality for saving miniscope files in run_vr_tuning.py

--- run_vr_tuning.py
+++ run_vr_tuning.py
@@ -11,7 +11,7 @@ from datetime import timedelta
 import paths
 from functions_osc import create_and_send
 from functions_recorder import initialize_projector, record_vr_trial_experiment, plot_inputs_vr, load_csv
-from functions_GUI import get_filename_suffix, replace_name_part
+from functions_GUI import get_filename_suffix, replace_name_part, replace_name_approx
 from vr_experiment_structures import VRTuningTrialStructure
 
 
@@ -35,7 +35,7 @@ trialsetName = csvName.replace('.csv', '.h5')
 # -- Load experiment parameters from excel file -- #
 
 # This is the parameter row that you want to use (matches excel row number)
-parameter_set = 2
+parameter_set = 3
 
 # Load the file
 all_params = pd.read_excel(paths.vrGratings_params_path, header=0, dtype=object)
@@ -77,11 +77,6 @@ try:
 except TypeError:
     pass
 
-# Create the H5 file to store the params
-with pd.HDFStore(trialsetName) as sess:
-    sess['trial_set'] = trials
-    sess['params'] = session_params
-
 # Put all of this in a class to be processed
 session = VRTuningTrialStructure(trials, trial_duration, isi)
 
@@ -108,6 +103,11 @@ duration, current_path_sync = record_vr_trial_experiment(session, my_device, pat
 # -- shutdown subprocesses -- #
 print("End Session")
 
+# Create the H5 file to store the params
+with pd.HDFStore(trialsetName) as sess:
+    sess['trial_set'] = trials
+    sess['params'] = session_params
+
 # close the opened applications
 create_and_send(paths.bonsai_ip, paths.bonsai_port, paths.bonsai_address, [1])
 create_and_send(paths.unity_ip, paths.unity_in_port, paths.unity_address, [1])
@@ -131,13 +131,21 @@ suffix = get_filename_suffix()
 
 # add the suffix to all the file names
 file_list = [csvName, videoName, trialsetName, current_path_sync]
-failed_files, _ = replace_name_part(file_list, 'suffix', suffix)
+failed_files, new_names = replace_name_part(file_list, 'suffix', suffix)
 print(failed_files)
 
-# do the same for the bonsai files
+# move the unity/motive file to the target folder
 unity_file = [paths.unity_temp_path]
-failed_unity, new_names = replace_name_part(unity_file, paths.unity_temp_path, join(paths.vr_path, 'suffix.txt'))
+failed_unity, new_unity_name = replace_name_part(unity_file, paths.unity_temp_path, join(paths.vr_path, 'suffix.txt'))
 print(failed_unity)
 
-failed_unity, _ = replace_name_part(new_names, 'suffix', '_'.join((time_name, exp_type, suffix)))
+# rename the unity/motive file to have the correct suffix
+failed_unity, _ = replace_name_part(new_unity_name, 'suffix', '_'.join((time_name, exp_type, suffix)))
 print(failed_unity)
+
+# Rename the miniscope .tif file and move it to the target folder
+# grab the csv path and change the extension
+new_tif_name = new_names[0].replace('.csv', '.tif')
+# TODO: test this functionality
+# add the matching name to the miniscope file (grabbing the file with the closest creation time, within 100 seconds)
+replace_name_approx(paths.doric_path, new_names[0], new_tif_name, threshold=100, extension='.tif')
